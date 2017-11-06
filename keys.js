1706002ee251ccd7abd728d09e2cb2bc37c37a5a
Alferguson <alferguson@berkeley.edu>
2017-11-06 09:08:13 -0800

    modularization

--- keys.js
+++ keys.js
@@ -1,5 +1,3 @@
-console.log('this is loaded');
-
 // twitter
 var twitterKeys = {
   consumer_key: '6cKvbWdZwhEfWaNPsGjqUvEOr',
@@ -8,14 +6,13 @@ var twitterKeys = {
   access_token_secret: 'MjDfHVLfToxFiy33v5tqsvqXXVWVoF6XHsESJ5FAt4kj2',
 }
 
-module.exports = twitterKeys;
-
-
 // spotify
-
 var spotifyKeys = {
   id: '66356ad8405848b6bdc681a2f625e7ad',
   secret: 'b85d02b246b14d65b1ab441746705aa7'
-};
+}
  
-module.exports = spotifyKeys;
+module.exports = {
+	twitterKeys: twitterKeys,
+	spotifyKeys: spotifyKeys
+}	
--- liri.js
+++ liri.js
@@ -5,14 +5,15 @@ var dataArr = [];
 var Spotify = require('node-spotify-api');
 var request = require('request');
 var fs = require('fs');
+var keys = require("./keys.js");
 
 // twitter function
 var twitterFunc = function() {
 	var twitter = new Twitter ({
-	  	consumer_key: '6cKvbWdZwhEfWaNPsGjqUvEOr',
-	  	consumer_secret: 'HN2OMSxEto4YnLT0HSlGTuIv1DuZscJzmPauBR4xKO8M1yb2zp',
-	  	access_token_key: '900160119119331328-hbzm6J3ltXdgWqRuSUbTJ7wfHgTc86w',
-	  	access_token_secret: 'MjDfHVLfToxFiy33v5tqsvqXXVWVoF6XHsESJ5FAt4kj2'
+	  	consumer_key: keys.twitterKeys.consumer_key,
+	  	consumer_secret: keys.twitterKeys.consumer_secret,
+	  	access_token_key: keys.twitterKeys.access_token_key,
+	  	access_token_secret: keys.twitterKeys.access_token_secret
 	});
 	// parameters for twitter to select the dril account and limit to 10 posts 
 	var params = {
@@ -24,7 +25,7 @@ var twitterFunc = function() {
 	  	if (!error && response.statusCode === 200) {
 	  		for (var i=0; i<tweets.length; i++) {
 	    		console.log(tweets[i].text);
-	    		console.log("-----------------");
+	    		console.log('-----------------');
 	    		dataArr.push(tweets[i].text);
 	    	}	
 	  	}
@@ -41,11 +42,11 @@ var spotifyFunc = function() {
 	spotifySong = inputArr;
 	// if user doesn't input a song, default to who let the dogs out
 	if (spotifySong == '') {
-		spotifySong = "Who let the dogs out?";
+		spotifySong = 'Who let the dogs out?';
 	}
 	var spotify = new Spotify ({
-	  	id: '66356ad8405848b6bdc681a2f625e7ad',
-		secret: 'b85d02b246b14d65b1ab441746705aa7'
+	  	id: keys.spotifyKeys.id,
+		secret: keys.spotifyKeys.secret
 	});
 	spotify.search ({ 
 		type: 'track', 
@@ -60,12 +61,17 @@ var spotifyFunc = function() {
 				console.log(data.tracks.items[i].artists[0].name);
 				// name
 				console.log(data.tracks.items[i].name);
-				// preview link to song
-				console.log(data.tracks.items[i].preview_url);
+				// preview link to song, there are many times that it returns null so I used an if func for that
+				if (data.tracks.items[i].preview_url === null) {
+					console.log("There is no preview url for this song unfortunately");
+				}
+				else {	
+					console.log(data.tracks.items[i].preview_url);
+				}	
 				// album song is from 
 				console.log(data.tracks.items[i].album.name);
 				// divider
-				console.log("----------------------");
+				console.log('---------------------');
 				dataArr.push(
 					data.tracks.items[i].artists[0].name, 
 					data.tracks.items[i].name,
@@ -86,7 +92,7 @@ var OMDBfunc = function() {
 	if (movieName == '') {
 		movieName = 'Trainspotting';
 	}
-	request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+	request('http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=40e9cece', function(error, response, body) {
 	  	if (!error && response.statusCode === 200) {
 		    console.log(JSON.parse(body).Title);
 		    console.log(JSON.parse(body).Year);
@@ -96,7 +102,7 @@ var OMDBfunc = function() {
 		    console.log(JSON.parse(body).Language);
 		    console.log(JSON.parse(body).Plot);
 		    console.log(JSON.parse(body).Actors);
-		    console.log("This is unrelated but you should def watch Trainspotting");
+		    console.log('This is unrelated but you should def watch Trainspotting');
 		    dataArr.push(
 		    	JSON.parse(body).Title,
 			    JSON.parse(body).Year,
@@ -114,7 +120,7 @@ var OMDBfunc = function() {
 
 // do what it says function
 var doWhatItSaysFunc = function() {
-	fs.readFile("random.txt", "utf8", function(error, data) {
+	fs.readFile('random.txt', 'utf8', function(error, data) {
 		if (error) {
 			return console.log(error);
 		}
